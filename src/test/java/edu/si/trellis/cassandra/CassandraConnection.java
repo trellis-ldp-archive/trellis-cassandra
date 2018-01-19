917688ab3be44d617451e36f764234f050111245
ajs6f <ajs6f@apache.org>
2018-01-19 11:48:19 -0500

    Use JUnit Rule instead of Before/AfterClass

---
+++ src/test/java/edu/si/trellis/cassandra/CassandraConnection.java
@@ -0,0 +1,39 @@
+package edu.si.trellis.cassandra;
+
+import static com.datastax.driver.core.Cluster.builder;
+import static edu.si.trellis.cassandra.DatasetCodec.datasetCodec;
+import static edu.si.trellis.cassandra.IRICodec.iriCodec;
+
+import org.junit.rules.ExternalResource;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.extras.codecs.jdk8.InstantCodec;
+
+class CassandraConnection extends ExternalResource {
+
+    protected Cluster cluster;
+    protected Session session;
+    public CassandraResourceService service;
+    private final int port;
+    private final String contact;
+
+    public CassandraConnection(String contact, int port) {
+        this.contact = contact;
+        this.port = port;
+    }
+
+    @Override
+    protected void before() {
+        cluster = builder().addContactPoint(contact).withPort(port).build();
+        cluster.getConfiguration().getCodecRegistry().register(iriCodec, datasetCodec, InstantCodec.instance);
+        session = cluster.connect(CassandraResourceServiceIT.TEST_KEYSPACE);
+        service = new CassandraResourceService(session);
+    }
+
+    @Override
+    protected void after() {
+        session.close();
+        cluster.close();
+    }
+}
--- src/test/java/edu/si/trellis/cassandra/CassandraResourceServiceIT.java
+++ src/test/java/edu/si/trellis/cassandra/CassandraResourceServiceIT.java
@@ -1,11 +1,10 @@
 package edu.si.trellis.cassandra;
 
-import static edu.si.trellis.cassandra.DatasetCodec.datasetCodec;
-import static edu.si.trellis.cassandra.IRICodec.iriCodec;
 import static org.trellisldp.vocabulary.RDF.type;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
 
 import org.apache.commons.rdf.api.Dataset;
 import org.apache.commons.rdf.api.IRI;
@@ -13,44 +12,25 @@ import org.apache.commons.rdf.api.Quad;
 import org.apache.commons.rdf.api.RDF;
 import org.apache.commons.rdf.api.Triple;
 import org.apache.commons.rdf.simple.SimpleRDF;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.trellisldp.api.Resource;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.extras.codecs.jdk8.InstantCodec;
-
 public class CassandraResourceServiceIT extends Assert {
 
-    private static final String TEST_KEYSPACE = System.getProperty("test.keyspace", "test");
+    static final String TEST_KEYSPACE = System.getProperty("test.keyspace", "test");
 
     private static final Logger log = LoggerFactory.getLogger(CassandraResourceServiceIT.class);
 
     protected static int port = Integer.getInteger("cassandra.nativeTransportPort");
 
-    protected static Cluster cluster;
-    protected static Session session;
-    protected static CassandraResourceService service;
     protected RDF rdfFactory = new SimpleRDF();
 
-    @BeforeClass
-    public static void setUp() {
-        cluster = Cluster.builder().addContactPoint("127.0.0.1").withPort(port).build();
-        cluster.getConfiguration().getCodecRegistry().register(iriCodec, datasetCodec, InstantCodec.instance);
-        session = cluster.connect(TEST_KEYSPACE);
-        service = new CassandraResourceService(session);
-    }
-
-    @AfterClass
-    public static void tearDown() {
-        session.close();
-        cluster.close();
-    }
+    @ClassRule
+    public static final CassandraConnection cassandraConnection = new CassandraConnection("127.0.0.1", port);
 
     @Test
     public void test() throws InterruptedException, ExecutionException {
@@ -59,15 +39,15 @@ public class CassandraResourceServiceIT extends Assert {
         Dataset quads = rdfFactory.createDataset();
         Quad quad = rdfFactory.createQuad(id, ixnModel, id, ixnModel);
         quads.add(quad);
-        CompletableFuture<Boolean> put = service.put(id, ixnModel, quads);
+        CompletableFuture<Boolean> put = cassandraConnection.service.put(id, ixnModel, quads);
         assertTrue(put.get());
-        Resource resource = service.get(id).orElseThrow(() -> new AssertionError("Failed to retrieve resource!"));
+        Resource resource = cassandraConnection.service.get(id).orElseThrow(MISSING_RESOURCE);
         assertEquals(id, resource.getIdentifier());
         assertEquals(ixnModel, resource.getInteractionModel());
         Quad firstQuad = resource.stream().findFirst().orElseThrow(() -> new AssertionError("Failed to find quad!"));
         assertEquals(quad, firstQuad);
     }
-    
+
     @Test
     public void testScan() throws InterruptedException, ExecutionException {
         IRI id = createIRI("http://example.com/id2");
@@ -75,10 +55,10 @@ public class CassandraResourceServiceIT extends Assert {
         Dataset quads = rdfFactory.createDataset();
         Quad quad = rdfFactory.createQuad(id, ixnModel, id, ixnModel);
         quads.add(quad);
-        CompletableFuture<Boolean> put = service.put(id, ixnModel, quads);
+        CompletableFuture<Boolean> put = cassandraConnection.service.put(id, ixnModel, quads);
         assertTrue(put.get());
-        assertEquals(1,service.scan().count());
-        Triple triple = service.scan().findFirst().orElseThrow(() -> new AssertionError("Failed to retrieve resource!"));
+        assertEquals(1, cassandraConnection.service.scan().count());
+        Triple triple = cassandraConnection.service.scan().findFirst().orElseThrow(MISSING_RESOURCE);
         assertEquals(id, triple.getSubject());
         assertEquals(type, triple.getPredicate());
         assertEquals(ixnModel, triple.getObject());
@@ -87,4 +67,7 @@ public class CassandraResourceServiceIT extends Assert {
     private IRI createIRI(String iri) {
         return rdfFactory.createIRI(iri);
     }
+
+    private static final Supplier<Error> MISSING_RESOURCE = () -> new AssertionError("Failed to retrieve resource!");
+
 }
