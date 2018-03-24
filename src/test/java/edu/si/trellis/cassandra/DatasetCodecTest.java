810ead145f83c2668e75c1fb6b21988cb0e85116
ajs6f <ajs6f@apache.org>
2018-03-24 16:30:19 -0400

    First tests for DatasetCodec

---
+++ src/test/java/edu/si/trellis/cassandra/DatasetCodecTest.java
@@ -0,0 +1,55 @@
+package edu.si.trellis.cassandra;
+
+import com.datastax.driver.core.exceptions.InvalidTypeException;
+
+import org.apache.commons.rdf.api.BlankNodeOrIRI;
+import org.apache.commons.rdf.api.Dataset;
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.Quad;
+import org.apache.commons.rdf.api.RDF;
+import org.apache.commons.rdf.api.RDFTerm;
+import org.apache.commons.rdf.simple.SimpleRDF;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DatasetCodecTest extends Assert {
+
+    private static final RDF rdf = new SimpleRDF();
+
+    @Test(expected = InvalidTypeException.class)
+    public void badParse() {
+        new DatasetCodec().parse("SGDF   &&$$$dfshgou;sdfhgoudfhogh");
+    }
+
+    @Test
+    public void testParse() throws Exception {
+        String nQuad1 = "<s> <p> <o> <g> .";
+        Quad q1 = quad(iri("g"), iri("s"), iri("p"), iri("o"));
+        String nQuad2 = "<s1> <p1> \"foo\" .";
+        Quad q2 = quad(null, iri("s1"), iri("p1"), rdf.createLiteral("foo"));
+        String nQuad3 = "<s> <p> <o> <g2> .";
+        Quad q3 = quad(iri("g2"), iri("s"), iri("p"), iri("o"));
+        String nQuads = String.join("\n", nQuad1, nQuad2, nQuad3);
+        try (Dataset dataset = new DatasetCodec().parse(nQuads)) {
+            assertEquals(3, dataset.size());
+            for (Quad q : new Quad[] { q1, q2, q3 })
+                assertTrue(dataset.contains(q));
+        }
+    }
+
+    @Test
+    public void nullForNull() {
+        assertEquals(0, new DatasetCodec().parse(null).size());
+        assertEquals(null, new DatasetCodec().format(null));
+        assertEquals(null, new DatasetCodec().serialize(null, null));
+        assertEquals(0, new DatasetCodec().deserialize(null, null).size());
+    }
+
+    private Quad quad(BlankNodeOrIRI g, BlankNodeOrIRI s, IRI p, RDFTerm o) {
+        return rdf.createQuad(g, s, p, o);
+    }
+
+    private IRI iri(String v) {
+        return rdf.createIRI(v);
+    }
+}
