365b6c8462d892e6db6a9bf7ef8bc66ddcb13dd9
ajs6f <ajs6f@apache.org>
2018-07-17 14:01:55 -0400

    Moar tests

--- src/test/java/edu/si/trellis/cassandra/DatasetCodecTest.java
+++ src/test/java/edu/si/trellis/cassandra/DatasetCodecTest.java
@@ -1,8 +1,12 @@
 package edu.si.trellis.cassandra;
 
 import static edu.si.trellis.cassandra.DatasetCodec.datasetCodec;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.datastax.driver.core.exceptions.InvalidTypeException;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.rdf.api.BlankNodeOrIRI;
 import org.apache.commons.rdf.api.Dataset;
@@ -40,9 +44,43 @@ public class DatasetCodecTest extends Assert {
     }
 
     @Test
-    public void nullForNull() {
+    public void testDeserialize() throws Exception {
+        String nQuad1 = "<s> <p> <o> <g> .";
+        Quad q1 = quad(iri("g"), iri("s"), iri("p"), iri("o"));
+        String nQuad2 = "<s1> <p1> \"foo\" .";
+        Quad q2 = quad(null, iri("s1"), iri("p1"), rdf.createLiteral("foo"));
+        String nQuad3 = "<s> <p> <o> <g2> .";
+        Quad q3 = quad(iri("g2"), iri("s"), iri("p"), iri("o"));
+        ByteBuffer nQuads = ByteBuffer.wrap(String.join("\n", nQuad1, nQuad2, nQuad3).getBytes(UTF_8));
+        try (Dataset dataset = datasetCodec.deserialize(nQuads, null)) {
+            assertEquals(3, dataset.size());
+            for (Quad q : new Quad[] { q1, q2, q3 })
+                assertTrue(dataset.contains(q));
+        }
+    }
+
+    @Test
+    public void testFormat() throws Exception {
+        String nQuad = "<s> <p> <o> <g> .";
+        Quad q = quad(iri("g"), iri("s"), iri("p"), iri("o"));
+        try (Dataset dataset = rdf.createDataset()) {
+            dataset.add(q);
+            String nQuads = datasetCodec.format(dataset);
+            assertEquals(1, dataset.size());
+            assertEquals(nQuad, nQuads.trim());
+        }
+    }
+
+    @Test
+    public void edgeCases() throws Exception {
         assertEquals(0, datasetCodec.parse(null).size());
         assertEquals(null, datasetCodec.format(null));
+        try (Dataset empty = rdf.createDataset()) {
+            assertEquals(null, datasetCodec.format(empty));
+        }
+        try (Dataset empty = rdf.createDataset()) {
+            assertEquals(null, datasetCodec.serialize(empty, null));
+        }
         assertEquals(null, datasetCodec.serialize(null, null));
         assertEquals(0, datasetCodec.deserialize(null, null).size());
     }
