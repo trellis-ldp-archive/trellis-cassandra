7e38cf6893e9bad192d8149c222c9196c5833aee
ajs6f <ajs6f@apache.org>
2018-07-27 13:29:54 -0400

    More tests

--- src/test/java/edu/si/trellis/cassandra/ByteBufferInputStreamTest.java
+++ src/test/java/edu/si/trellis/cassandra/ByteBufferInputStreamTest.java
@@ -12,9 +12,13 @@ public class ByteBufferInputStreamTest extends Assert {
     private final byte[] testByteArray = new byte[] { 1, 2, 3, 4, 3, 2, 1 };
     private final ByteBuffer testData = ByteBuffer.wrap(testByteArray);
 
+    private ByteBuffer testData() {
+        return testData.asReadOnlyBuffer();
+    }
+
     @Test(expected = IOException.class)
     public void cantResetBeyondLimit() throws IOException {
-        ByteBufferInputStream stream = new ByteBufferInputStream(testData.asReadOnlyBuffer());
+        ByteBufferInputStream stream = new ByteBufferInputStream(testData());
         stream.mark(3);
         stream.read(new byte[8]);
         stream.reset();
@@ -22,19 +26,19 @@ public class ByteBufferInputStreamTest extends Assert {
 
     @Test
     public void availableWorks() throws IOException {
-        ByteBufferInputStream stream = new ByteBufferInputStream(testData.asReadOnlyBuffer());
+        ByteBufferInputStream stream = new ByteBufferInputStream(testData());
         assertEquals(7, stream.available());
     }
 
     @Test
     public void supportsMark() {
-        ByteBufferInputStream stream = new ByteBufferInputStream(testData.asReadOnlyBuffer());
+        ByteBufferInputStream stream = new ByteBufferInputStream(testData());
         assertTrue(stream.markSupported());
     }
 
     @Test
     public void noMarkMeansResetTo0() throws IOException {
-        ByteBufferInputStream stream = new ByteBufferInputStream(testData.asReadOnlyBuffer());
+        ByteBufferInputStream stream = new ByteBufferInputStream(testData());
         byte[] answer = new byte[testByteArray.length];
         stream.read(new byte[3]);
         stream.reset();
@@ -51,7 +55,7 @@ public class ByteBufferInputStreamTest extends Assert {
 
     @Test
     public void skipWorks() throws IOException {
-        ByteBufferInputStream stream = new ByteBufferInputStream(testData.asReadOnlyBuffer());
+        ByteBufferInputStream stream = new ByteBufferInputStream(testData());
         byte[] answer = new byte[testByteArray.length];
         stream.skip(3);
         assertEquals(4, stream.read(answer));
@@ -59,8 +63,7 @@ public class ByteBufferInputStreamTest extends Assert {
 
     @Test
     public void readWorks() throws IOException {
-        ByteBufferInputStream stream = new ByteBufferInputStream(testData.asReadOnlyBuffer());
-        for (int i : testByteArray)
-            assertEquals(i, stream.read());
+        ByteBufferInputStream stream = new ByteBufferInputStream(testData());
+        for (int i : testByteArray) assertEquals(i, stream.read());
     }
 }
