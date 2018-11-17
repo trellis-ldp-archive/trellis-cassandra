d216ba33b21cf371c85f94ee23eb23ec236c28dc
ajs6f <ajs6f@apache.org>
2018-11-17 11:27:57 -0500

    Still trying with CircleCI

--- src/test/resources/load.sh
+++ src/test/resources/load.sh
@@ -1,6 +1,6 @@
-until nodetool status; do
-    sleep 5
-    echo "Waiting for Cassandra..." 
-done
+#until nodetool status; do
+#    sleep 5
+#    echo "Waiting for Cassandra..." 
+#done
 sleep 5
 cqlsh -f src/test/resources/load.cql
