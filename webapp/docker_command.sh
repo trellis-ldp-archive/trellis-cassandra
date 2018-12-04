4e4ee71e551d88189e34bebb45bc699f66373922
Gregory Jansen <gregory.jansen@gmail.com>
2018-12-04 14:58:37 -0500

    Adds a docker-specific logback configuration and location Adds required command-line parameters for port 8080 and IPv4 networking

--- webapp/docker_command.sh
+++ webapp/docker_command.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-sleep 20
-java -jar webapp-hollow-thorntail.jar webapp.war
+java -Dswarm.http.port=8080 -Dorg.jboss.logging.provider=slf4j -Dlogback.configurationFile=/logback-docker.xml -Djava.net.preferIPv4Stack=true -jar webapp-hollow-thorntail.jar webapp.war
