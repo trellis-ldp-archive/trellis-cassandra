c58b4ac4f064d38d1ea876315fe4adb72c84b89f
ajs6f <ajs6f@apache.org>
2019-03-26 13:13:50 -0400

    Catching up with new Trellis Resource API

--- impl/src/main/java/edu/si/trellis/CassandraResource.java
+++ impl/src/main/java/edu/si/trellis/CassandraResource.java
@@ -4,7 +4,6 @@ import static com.datastax.driver.core.utils.UUIDs.unixTimestamp;
 import static java.util.stream.Stream.concat;
 import static org.slf4j.LoggerFactory.getLogger;
 import static org.trellisldp.api.BinaryMetadata.builder;
-import static org.trellisldp.api.TrellisUtils.toQuad;
 import static org.trellisldp.vocabulary.LDP.*;
 
 import com.datastax.driver.core.Row;
@@ -23,7 +22,6 @@ import java.util.stream.StreamSupport;
 import org.apache.commons.rdf.api.IRI;
 import org.apache.commons.rdf.api.Quad;
 import org.apache.commons.rdf.api.RDF;
-import org.apache.commons.rdf.api.Triple;
 import org.slf4j.Logger;
 import org.trellisldp.api.BinaryMetadata;
 import org.trellisldp.api.Resource;
@@ -51,6 +49,8 @@ class CassandraResource implements Resource {
     private final BasicContainment bcontainment;
 
     private final long createdMs;
+
+    private static final RDF rdfFactory = TrellisUtils.getInstance();
 
     public CassandraResource(IRI id, IRI ixnModel, boolean hasAcl, IRI binaryIdentifier, String mimeType, IRI container,
                     Instant modified, UUID created, ImmutableRetrieve immutable, MutableRetrieve mutable,
@@ -120,24 +120,29 @@ class CassandraResource implements Resource {
     @Override
     public Stream<Quad> stream() {
         log.trace("Retrieving quad stream for resource {}", getIdentifier());
-        Stream<Quad> mutableQuads = mutable.execute(getIdentifier(), createdMs);
-        Stream<Quad> immutableQuads = immutable.execute(getIdentifier());
-        Stream<Quad> quads = concat(mutableQuads, immutableQuads);
+        Stream<Quad> quads = concat(mutableQuads(), immutableQuads());
         if (isContainer) {
-            Stream<Quad> containmentQuads = basicContainmentTriples().map(toQuad(PreferContainment));
+            Stream<Quad> containmentQuads = basicContainmentQuads();
             quads = concat(quads, containmentQuads);
         }
         return quads;
     }
 
-    private Stream<Triple> basicContainmentTriples() {
-        RDF rdfFactory = TrellisUtils.getInstance();
-        Spliterator<Row> rows = bcontainment.execute(getIdentifier()).spliterator();
-        Stream<IRI> contained = StreamSupport.stream(rows, false).map(r -> r.get("contained", IRI.class));
-        return contained.map(cont -> rdfFactory.createTriple(getIdentifier(), contains, cont))
-                        .peek(t -> log.trace("Built containment triple: {}", t));
+    private Stream<Quad> mutableQuads() {
+        return mutable.execute(getIdentifier(), createdMs);
     }
 
+    private Stream<Quad> immutableQuads() {
+        return immutable.execute(getIdentifier());
+    }
+
+    private Stream<Quad> basicContainmentQuads() {
+        Spliterator<Row> rows = bcontainment.execute(getIdentifier()).spliterator();
+        Stream<IRI> contained = StreamSupport.stream(rows, false).map(r -> r.get("contained", IRI.class));
+        return contained.map(c -> rdfFactory.createQuad(PreferContainment, getIdentifier(), contains, c))
+                        .peek(t -> log.trace("Built containment quad: {}", t));
+    }
+    
     /**
      * If there are mutable triples stored in the PreferContainment named graph, they will <i>not</i> be returned here.
      * Our assumption is that no user will intentionally use that URI as a name for a graph.
@@ -145,8 +150,8 @@ class CassandraResource implements Resource {
      * @see org.trellisldp.api.Resource#stream(org.apache.commons.rdf.api.IRI)
      */
     @Override
-    public Stream<Triple> stream(IRI graphName) {
-        if (graphName.equals(PreferContainment)) return basicContainmentTriples();
+    public Stream<Quad> stream(IRI graphName) {
+        if (graphName.equals(PreferContainment)) return basicContainmentQuads();
         return Resource.super.stream(graphName);
     }
 }
