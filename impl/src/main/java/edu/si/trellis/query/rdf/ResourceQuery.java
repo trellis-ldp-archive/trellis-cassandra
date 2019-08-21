92cbc56ba9c16231103abeeeb90e4073f745959b
ajs6f <ajs6f@apache.org>
2019-08-21 16:22:56 -0400

    Fully asynchronized RDF retrieval

--- impl/src/main/java/edu/si/trellis/query/rdf/ResourceQuery.java
+++ impl/src/main/java/edu/si/trellis/query/rdf/ResourceQuery.java
@@ -1,15 +1,17 @@
 package edu.si.trellis.query.rdf;
 
+import static java.util.stream.StreamSupport.stream;
+
 import com.datastax.driver.core.ConsistencyLevel;
+import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.Statement;
 
 import edu.si.trellis.query.CassandraQuery;
 
-import java.util.Spliterator;
+import java.util.concurrent.CompletionStage;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import org.apache.commons.rdf.api.Dataset;
 import org.apache.commons.rdf.api.Quad;
@@ -31,9 +33,15 @@ abstract class ResourceQuery extends CassandraQuery {
         super(session, queryString, consistency);
     }
 
-    protected Stream<Quad> quads(final Statement boundStatement) {
-        final Spliterator<Row> rows = executeSyncRead(boundStatement).spliterator();
-        Stream<Dataset> datasets = StreamSupport.stream(rows, false).map(r -> r.get("quads", Dataset.class));
-        return datasets.flatMap(Dataset::stream);
+    protected CompletionStage<Stream<Quad>> quads(final Statement boundStatement) {
+        return executeRead(boundStatement)
+                        .thenApply(ResultSet::spliterator)
+                        .thenApply(r -> stream(r, false))
+                        .thenApply(row -> row.map(this::getDataset))
+                        .thenApply(r -> r.flatMap(Dataset::stream));
+    }
+
+    private Dataset getDataset(Row r) {
+        return r.get("quads", Dataset.class);
     }
 }
