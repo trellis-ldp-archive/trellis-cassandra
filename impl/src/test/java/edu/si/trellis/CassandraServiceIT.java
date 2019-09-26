4755e07737c128d400adb7a4baa416de29b522a5
Aaron Coburn <acoburn@apache.org>
2019-09-26 10:51:29 -0400

    Align with upstream API

--- impl/src/test/java/edu/si/trellis/CassandraServiceIT.java
+++ impl/src/test/java/edu/si/trellis/CassandraServiceIT.java
@@ -1,8 +1,8 @@
 package edu.si.trellis;
 
 import static java.lang.Boolean.TRUE;
+import static java.time.Duration.ofSeconds;
 import static org.awaitility.Awaitility.await;
-import static org.awaitility.Duration.TWO_SECONDS;
 
 import org.apache.commons.rdf.api.IRI;
 import org.apache.commons.rdf.api.RDF;
@@ -21,6 +21,6 @@ class CassandraServiceIT {
     }
 
     protected void waitTwoSeconds() {
-        await().pollDelay(TWO_SECONDS).until(() -> TRUE);
+        await().pollDelay(ofSeconds(2)).until(() -> TRUE);
     }
 }
